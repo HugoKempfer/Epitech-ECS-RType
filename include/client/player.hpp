168b9a9629a0e2182366ee88103816a73789b99a
Stephane <stephanedubedat666@gmail.com>
2019-12-22 10:54:30 +0100

    feat(PlayerCollision): the feature is complete, the bounds checking is made after any move event

--- include/client/player.hpp
+++ include/client/player.hpp
@@ -11,6 +11,8 @@
 #include "engine/prelude.hpp"
 #include "render/events.hpp"
 #include "engine/built_in/position.hpp"
+#include "render/ressources.hpp"
+#include "render/sprite_component.hpp"
 
 using Engine::Component;
 using Engine::World;
@@ -28,13 +30,17 @@ class PlayerSystem :
 	public Engine::System,
 	public Engine::EventHandler<KeystrokeEvent>
 {
+	using Window = Engine::Render::WindowRessource;
 public:
 	PlayerSystem() = delete;
 	PlayerSystem(World &world) : System(world,
 			{world.uuidCtx.get<PlayerComponent>(),
-			world.uuidCtx.get<Engine::PositionComponent>()
+			world.uuidCtx.get<Engine::PositionComponent>(),
+			world.uuidCtx.get<Engine::Render::SpriteComponent>()
 			},
-			{}),
+			{
+				world.uuidCtx.get<Window>()
+			}),
 		EventHandler<KeystrokeEvent>(world)
 		{}
 
@@ -44,6 +50,7 @@ public:
 
 private:
 	void handleMoves();
+	void adjustPositionToWindowBounds(Engine::PositionComponent &player_pos);
 	void shoot();
 	enum Arrows {LEFT, UP, DOWN, RIGHT, SIZE};
 	bool _arrows[Arrows::SIZE] = {false};
